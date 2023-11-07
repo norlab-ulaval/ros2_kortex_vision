7afb0a1d1756a9b9575fe70670969d7a07d55fa1
Erik Holum <erik.holum@picknik.ai>
2023-11-07 08:44:46 -0700

    Launch depth registration nodes

--- launch/kinova_vision.launch.py
+++ launch/kinova_vision.launch.py
@@ -2,7 +2,8 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, OpaqueFunction, LogInfo
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
+from launch_ros.actions import Node, ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
 
 import yaml
 
@@ -14,13 +15,13 @@ configurable_parameters = [
     {"name": "depth_frame_id", "default": "camera_depth_frame", "description": "Depth camera frame identifier"},
     {"name": "color_camera_info_url", "default": "", "description": "URL of custom calibration file for color camera. See camera_info_manager docs for calibration URL details"},
     {"name": "depth_camera_info_url", "default": "", "description": "URL of custom calibration file for depth camera. See camera_info_manager docs for calibration URL details"},
-    {"name": "depth_registration", "default": "false", "description": "Hardware depth registration"},
     {"name": "depth_rtsp_element_config", "default": "depth latency=30", "description": "RTSP element configuration for depth stream"},
     {"name": "depth_rtp_depay_element_config", "default": "rtpgstdepay", "description": "RTP element configuration for depth stream"},
     {"name": "color_rtsp_element_config", "default": "color latency=30", "description": "RTSP element configuration for color stream"},
     {"name": "color_rtp_depay_element_config", "default": "rtph264depay", "description": "RTP element configuration for color stream"},
     {"name": "launch_color", "default": "true", "description": "Launch the color image node"},
     {"name": "launch_depth", "default": "true", "description": "Launch the depth image node"},
+    {"name": "depth_registration", "default": "false", "description": "Hardware depth registration"},
 ]
 
 
@@ -64,6 +65,7 @@ def launch_setup(context, *args, **kwargs):
         condition=IfCondition(LaunchConfiguration('launch_depth')),
     )
 
+    # Color node configuration
     color_node = Node(
         package="kinova_vision",
         namespace=LaunchConfiguration('camera'),
@@ -87,6 +89,41 @@ def launch_setup(context, *args, **kwargs):
             ("image_raw/theora", "color/image_raw/theora"),
         ],
         condition=IfCondition(LaunchConfiguration('launch_color')),
+    )
+
+    # Conditionally launch tools to publish registered depth images
+    depth_image_proc = ComposableNodeContainer(
+        name="registered_depth_images",
+        namespace=LaunchConfiguration('camera'),
+        package="rclcpp_components",
+        executable="component_container",
+        composable_node_descriptions=[
+            ComposableNode(
+                package="depth_image_proc",
+                plugin="depth_image_proc::RegisterNode",
+                name="register_node",
+                namespace=LaunchConfiguration('camera'),
+                remappings=[
+                    ("rgb/camera_info", "color/camera_info"),
+                    ("depth/camera_info", "depth/camera_info"),
+                    ("depth/image_rect", "depth/image_raw"),
+                ],
+            ),
+            ComposableNode(
+                package="depth_image_proc",
+                plugin="depth_image_proc::PointCloudXyzrgbNode",
+                name=("point_cloud_xyzrgb"),
+        namespace=LaunchConfiguration('camera'),
+                remappings=[
+                    ("rgb/camera_info", "color/camera_info"),
+                    ("depth/camera_info", "depth/camera_info"),
+                    ("depth/image_rect", "depth/image_raw"),
+                    ("points", "depth/color/points"),
+                ],
+            ),
+        ],
+        output="both",
+        condition=IfCondition(LaunchConfiguration('depth_registration')),
     )
 
     # Static Transformation Publishers
@@ -114,7 +151,7 @@ def launch_setup(context, *args, **kwargs):
         condition=IfCondition(LaunchConfiguration('launch_color')),
     )
 
-    return [depth_node, color_node, camera_depth_tf_publisher, camera_color_tf_publisher]
+    return [depth_node, color_node, camera_depth_tf_publisher, camera_color_tf_publisher, depth_image_proc]
 
 
 def generate_launch_description():
